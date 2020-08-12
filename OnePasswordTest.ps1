8f933dfbe90ed52cbae6744275f92b64b84f017e
Marek.Dobes <marek.dobes@ibm.com>
2020-08-12 21:54:37 +0200

    Clean output

--- OnePasswordTest.ps1
+++ OnePasswordTest.ps1
@@ -1,12 +1,25 @@
 #requires -version 3
-#[CmdletBinding()]
+
 $Session_Token = Invoke-Expression "C://op/op.exe signin ibm --raw"
 $allitems = "C://op/op.exe get item - --fields password username title"
 $allLogins = 'C://op/op list items --categories "Login" --session ' + $Session_Token + ' | C://op/op get item - --fields title,username,password --session ' + $Session_Token
-#Invoke-Expression $allitems
+
 #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_arrays?view=powershell-7
-#Invoke-Expression "DEADonepasswordnet1029"
-Write-Output($Session_Token)
-$Array = Invoke-Expression $allLogins
-Write-Output($Array)
-Write-Output($Array.GetType())
+
+
+#Write-Output($Session_Token)
+$ArrayOfLogins = Invoke-Expression $allLogins
+#Write-Output($ArrayOfLogins)
+#Write-Output($ArrayOfLogins.GetType())
+foreach ($i in $ArrayOfLogins) {
+    #Write-Output($i.GetType())
+    
+    $i = $i.replace('"',"")
+    $i = $i.Replace("{","")
+    $i = $i.replace('}',"")
+    $toarray = $i.split(",")
+    
+    foreach ($a in $toarray.Replace('"',"")) {
+        Write-Output($a)
+    }
+}
