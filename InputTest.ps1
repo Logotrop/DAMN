1b522e473db4a9848dcaa6cedc5ddab5b10f9d69
Marek.Dobes <marek.dobes@ibm.com>
2020-08-19 09:56:12 +0200

    Max wants zis

--- InputTest.ps1
+++ InputTest.ps1
@@ -8,8 +8,14 @@ start-sleep -Milliseconds 500
 Invoke-Expression [Microsoft.VisualBasic.Interaction]::AppActivate("Calc")
 Invoke-Expression [System.Windows.Forms.SendKeys]::SendWait("1{ADD}1=")
 #>
+$username=$args[0]
+$password=$args[1]
+$test=$args[2]
 
-
+$arguments = "https://sniaccess.mop.esni.ibm.com:900 France " + $username
 #https://stackoverflow.com/questions/24286055/user-input-for-external-program-in-powershell
 $Wscript = New-Object -com wscript.shell
-Start-Process -FilePath "C:\Program Files (x86)\Toxclient\toxclient.exe" -Argumentlist "https://sniaccess.mop.esni.ibm.com:900 France CZZZ03BC";Start-sleep -seconds 1;$wscript.SendKeys("15charactersistoolong!");$wscript.SendKeys("{Enter}")
+Start-Process -FilePath "C:\Program Files (x86)\Toxclient\toxclient.exe" -Argumentlist $arguments;
+Start-sleep -seconds 1;
+$wscript.SendKeys($password);
+$wscript.SendKeys("{Enter}")
