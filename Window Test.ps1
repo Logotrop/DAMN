2601c675f5d82d343705876b8028e9e11d5796d5
Marek.Dobes <marek.dobes@ibm.com>
2020-09-11 01:00:17 +0200

    Cleanup of junk

--- Window Test.ps1
+++
@@ -1,7 +0,0 @@
-#requires -version 3
-Import-Module .\AutoItX.psd1
-while ($True) {
-    Start-Sleep -Milliseconds 500
-    $winhandle = Get-AU3WinHandle -Title "France"
-    Show-AU3WinActivate -WinHandle $winhandle
-}
