e136c621611cc536bab3dd483e479fc94ecd038c
Marek.Dobes <marek.dobes@ibm.com>
2020-11-22 16:28:30 +0100

    Version V1.2.4

--- DAMN.ps1
+++ DAMN.ps1
@@ -1,10 +1,16 @@
 #requires -version 3
 Import-Module .\AX\AutoItX.psd1 #Import Automation Library
 #Import-Module .\GnuPg.psm1 #Import GpG encryption module
+
+#DO NOT USE THIS!!!
 $MasterPassword = ""
+if (Test-Path $pwd\Termsandconditions.txt) {
+    $MasterPassword = Get-Content -Path $pwd\Termsandconditions.txt -TotalCount 1 #Bypass for lazy people, until I implement encryption
+}
+
 Add-Type -assembly System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
-$CurrentVersion = "1.2.2"
+$CurrentVersion = "1.2.3"
 Out-File -FilePath $pwd\version.txt -Encoding ASCII -InputObject $CurrentVersion
 #Version Control
 $repo = "Logotrop/DAMN"
@@ -208,15 +214,18 @@ function WindowManager {
         #Get ID of Process from Window name
         $winhandle = Get-AU3WinHandle -Title $ProcessName
         # 1 = Window Open 0 = Not Open, also selects it
-        $isone = Show-AU3WinActivate -WinHandle $winhandle
+        $isone = Show-AU3WinActivate -WinHandle $winhandle # The overuse is because windows is not cooperating
         if ($isone -eq 1) {
             if ($username -ne "") {
+                Show-AU3WinActivate -WinHandle $winhandle
                 $wscript.SendKeys($username);   
             }
             if ($password -ne "") {
+                Show-AU3WinActivate -WinHandle $winhandle
                 $wscript.SendKeys($password);   
             }
             if ($NeedsEnter) {
+                Show-AU3WinActivate -WinHandle $winhandle
                 $wscript.SendKeys("{Enter}")   
             }
             #Breaks the loop if input passed
@@ -456,18 +465,28 @@ $Mainform.Text = 'DAMN'
 $Mainform.Size = New-Object System.Drawing.Size(300, 600)
 $Mainform.StartPosition = 'CenterScreen'
 $Mainform.FormBorderStyle = "FixedDialog"
+
 # Create IBM Notes Button
 $IBMnotesButton = New-Object System.Windows.Forms.Button
-$IBMnotesButton.Location = New-Object System.Drawing.Point(5, 30)
-$IBMnotesButton.Size = New-Object System.Drawing.Size(150, 40)
+$IBMnotesButton.Location = New-Object System.Drawing.Point(5, 15)
+$IBMnotesButton.Size = New-Object System.Drawing.Size(135, 40)
+$IBMnotesButton.FlatStyle = "Flat"
+$IBMnotesButton.FlatAppearance.BorderSize = 0;
+$IBMnotesButton.Font = New-Object System.Drawing.Font($IBMnotesButton.Font.FontFamily, 9.25, [System.Drawing.FontStyle]::Regular)
 $IBMnotesButton.Text = 'IBM Notes'
 $Mainform.AcceptButton = $IBMnotesButton
 $Mainform.Controls.Add($IBMnotesButton)
+
+#Appearance settings from IBMNotesButton
+
 # Create Tox Button
 if (Test-Path "C:\Program Files (x86)\Toxclient") {
     $ToxButton = New-Object System.Windows.Forms.Button
-    $ToxButton.Location = New-Object System.Drawing.Point(5, 75)
-    $ToxButton.Size = New-Object System.Drawing.Size(150, 40)
+    $ToxButton.Location = New-Object System.Drawing.Point(5, 60)
+    $ToxButton.Size = New-Object System.Drawing.Size(135, 40)
+    $ToxButton.FlatStyle = "Flat"
+    $ToxButton.FlatAppearance.BorderSize = 0;
+    $ToxButton.Font = $IBMnotesButton.Font
     $ToxButton.Text = 'TOX'
     $Mainform.AcceptButton = $ToxButton
     $Mainform.Controls.Add($ToxButton)
@@ -479,8 +498,11 @@ else {
 $BlueZzzPath = ($pwd).Path + "\BlueZzz"
 if (Test-Path $BlueZzzPath) {
     $BlueZzzButton = New-Object System.Windows.Forms.Button
-    $BlueZzzButton.Location = New-Object System.Drawing.Point(5, 120)
-    $BlueZzzButton.Size = New-Object System.Drawing.Size(150, 40)
+    $BlueZzzButton.Location = New-Object System.Drawing.Point(5, 105)
+    $BlueZzzButton.Size = New-Object System.Drawing.Size(135, 40)
+    $BlueZzzButton.FlatStyle = "Flat"
+    $BlueZzzButton.FlatAppearance.BorderSize = 0;
+    $BlueZzzButton.Font = $IBMnotesButton.Font
     $BlueZzzButton.Text = 'BlueZzz'
     $Mainform.AcceptButton = $BlueZzzButton
     $Mainform.Controls.Add($BlueZzzButton)
@@ -494,7 +516,7 @@ $PectPath = ($pwd).Path + "\PECTv1.5"
 if (Test-Path $PectPath) {
     #Not safe Label
     $Pectlabel = New-Object System.Windows.Forms.Label
-    $Pectlabel.Location = New-Object System.Drawing.Point(5, 165)
+    $Pectlabel.Location = New-Object System.Drawing.Point(5, 150)
     $Pectlabel.Size = New-Object System.Drawing.Size(280, 15)
     $Pectlabel.Text = "! Not Secure !"
     $Mainform.Controls.Add($Pectlabel)
@@ -502,8 +524,11 @@ if (Test-Path $PectPath) {
 
     # PECT Button
     $PectButton = New-Object System.Windows.Forms.Button
-    $PectButton.Location = New-Object System.Drawing.Point(5, 180)
-    $PectButton.Size = New-Object System.Drawing.Size(150, 40)
+    $PectButton.Location = New-Object System.Drawing.Point(5, 165)
+    $PectButton.Size = New-Object System.Drawing.Size(135, 40)
+    $PectButton.FlatStyle = "Flat"
+    $PectButton.FlatAppearance.BorderSize = 0;
+    $PectButton.Font = $IBMnotesButton.Font
     $PectButton.Text = 'PECT'
     $Mainform.AcceptButton = $PectButton
     $Mainform.Controls.Add($PectButton)
@@ -511,40 +536,88 @@ if (Test-Path $PectPath) {
 else {
     [System.Windows.Forms.MessageBox]::Show( "Please move BlueZzz Folder To this Folder" , "BlueZzz Not Found")
 }
-$Dark = 2
 $OnePasswordButton = New-Object System.Windows.Forms.Button
-$OnePasswordButton.Location = New-Object System.Drawing.Point(160, 30)
-$OnePasswordButton.Size = New-Object System.Drawing.Size(120, 85)
+$OnePasswordButton.Location = New-Object System.Drawing.Point(145, 15)
+$OnePasswordButton.Size = New-Object System.Drawing.Size(135, 85)
+$OnePasswordButton.FlatStyle = "Flat"
+$OnePasswordButton.FlatAppearance.BorderSize = 0;
+$OnePasswordButton.Font = $IBMnotesButton.Font
 $OnePasswordButton.Text = '1Password Master Pass'
 $Mainform.AcceptButton = $OnePasswordButton
 $Mainform.Controls.Add($OnePasswordButton)
+
 # Create Update button
 $UpdateButton = New-Object System.Windows.Forms.Button
-$UpdateButton.Location = New-Object System.Drawing.Point(160, 515)
-$UpdateButton.Size = New-Object System.Drawing.Size(120, 40)
+$UpdateButton.Location = New-Object System.Drawing.Point(145, 515)
+$UpdateButton.Size = New-Object System.Drawing.Size(135, 40)
+$UpdateButton.FlatStyle = "Flat"
+$UpdateButton.FlatAppearance.BorderSize = 0;
+$UpdateButton.Font = $IBMnotesButton.Font
 $UpdateButton.Text = 'Update passwords'
 $Mainform.AcceptButton = $UpdateButton
 $Mainform.Controls.Add($UpdateButton)
+
 # Super Secret
 $SuperSecret = New-Object System.Windows.Forms.Button
 $SuperSecret.Location = New-Object System.Drawing.Point(1800, 950)
-$SuperSecret.Size = New-Object System.Drawing.Size(120, 40)
+$SuperSecret.Size = New-Object System.Drawing.Size(135, 40)
+$SuperSecret.FlatStyle = "Flat"
+$SuperSecret.FlatAppearance.BorderSize = 0;
+$SuperSecret.Font = $IBMnotesButton.Font
 $SuperSecret.Text = 'Secret Setting'
 $Mainform.AcceptButton = $SuperSecret
 $Mainform.Controls.Add($SuperSecret)
+
 # Dark Mode
 $DarkMode = New-Object System.Windows.Forms.Button
 $DarkMode.Location = New-Object System.Drawing.Point(5, 515)
-$DarkMode.Size = New-Object System.Drawing.Size(120, 40)
+$DarkMode.Size = New-Object System.Drawing.Size(135, 40)
+$DarkMode.FlatStyle = "Flat"
+$DarkMode.FlatAppearance.BorderSize = 0;
+$DarkMode.Font = $IBMnotesButton.Font
 $DarkMode.Text = 'Dark Mode'
 $Mainform.AcceptButton = $DarkMode
 $Mainform.Controls.Add($DarkMode)
 
+# Handover Button
+$HandoverButton = New-Object System.Windows.Forms.Button
+$HandoverButton.Location = New-Object System.Drawing.Point(5, 470)
+$HandoverButton.Size = New-Object System.Drawing.Size(135, 40)
+$HandoverButton.FlatStyle = "Flat"
+$HandoverButton.FlatAppearance.BorderSize = 0;
+$HandoverButton.Font = $IBMnotesButton.Font
+$HandoverButton.Text = 'Handover'
+$Mainform.AcceptButton = $HandoverButton
+$Mainform.Controls.Add($HandoverButton)
+
+# Maximo Button
+$MaximoButton = New-Object System.Windows.Forms.Button
+$MaximoButton.Location = New-Object System.Drawing.Point(145, 470)
+$MaximoButton.Size = New-Object System.Drawing.Size(135, 40)
+$MaximoButton.FlatStyle = "Flat"
+$MaximoButton.FlatAppearance.BorderSize = 0;
+$MaximoButton.Font = $IBMnotesButton.Font
+$MaximoButton.Text = 'Maximo'
+$Mainform.AcceptButton = $MaximoButton
+$Mainform.Controls.Add($MaximoButton)
+
+# Checklist Button
+$ChecklistButton = New-Object System.Windows.Forms.Button
+$ChecklistButton.Location = New-Object System.Drawing.Point(5, 425)
+$ChecklistButton.Size = New-Object System.Drawing.Size(135, 40)
+$ChecklistButton.FlatStyle = "Flat"
+$ChecklistButton.FlatAppearance.BorderSize = 0;
+$ChecklistButton.Font = $IBMnotesButton.Font
+$ChecklistButton.Text = 'Checklist'
+$Mainform.AcceptButton = $ChecklistButton
+$Mainform.Controls.Add($ChecklistButton)
+
 $label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(5, 10)
-$label.Size = New-Object System.Drawing.Size(280, 20)
+$label.Location = New-Object System.Drawing.Point(5, 0)
+$label.Size = New-Object System.Drawing.Size(280, 15)
 $label.Text = 'Choose App to logon to:'
 $Mainform.Controls.Add($label)
+
 #Colors!
 $PectButton.BackColor = "LightBlue"
 $ToxButton.BackColor = "LightBlue"
@@ -553,7 +626,10 @@ $UpdateButton.BackColor = "LightBlue"
 $OnePasswordButton.BackColor = "LightBlue"
 $SuperSecret.BackColor = "LightBlue"
 $IBMnotesButton.BackColor = "LightBlue"
-$DarkMode.backColor = "LightBlue"
+$DarkMode.BackColor = "LightBlue"
+$HandoverButton.BackColor = "Violet"
+$MaximoButton.BackColor = "Violet"
+$ChecklistButton.BackColor = "Violet"
 
 
 # Event Handeling - Button Click
@@ -596,7 +672,14 @@ if (Test-Path $PectPath) {
 }
 $OnePasswordButton.Add_Click(
     {
-        WindowManager "Master" $MasterPassword
+        if ($MasterPassword -ne $null) {
+            WindowManager "Master" $MasterPassword
+        }
+        else {
+            $i = $Credentials.title.IndexOf("MasterPass")
+            WindowManager "Master" $Credentials[$i].password
+        }
+        
     }
 )
 
@@ -605,6 +688,25 @@ $UpdateButton.Add_Click(
         $Credentials = GetLogins
     }
 )
+
+$HandoverButton.Add_Click(
+    {
+        Start-Process 'https://bat.cz.ibm.com/handover'
+    }
+)
+
+$MaximoButton.Add_Click(
+    {
+        Start-Process 'https://esls3.eu.smi.ibm.com/maximo/ui/login'
+    }
+)
+
+$ChecklistButton.Add_Click(
+    {
+        Start-Process 'https://bat.cz.ibm.com/checklist'
+    }
+)
+
 $secretcodes = get-content $pwd\codes.txt
 $SuperSecret.Add_Click(
     {
@@ -634,6 +736,9 @@ $DarkMode.Add_Click(
             $SuperSecret.BackColor = "LightBlue"
             $IBMnotesButton.BackColor = "LightBlue"
             $DarkMode.backColor = "LightBlue"
+            $HandoverButton.BackColor = "Violet"
+            $MaximoButton.BackColor = "Violet"
+            $ChecklistButton.BackColor = "Violet"
             $Mainform.ForeColor = "Black"
         }
         else {
@@ -646,6 +751,9 @@ $DarkMode.Add_Click(
             $SuperSecret.BackColor = "Purple"
             $IBMnotesButton.BackColor = "DarkBlue"
             $DarkMode.backColor = "DarkBlue"
+            $HandoverButton.BackColor = "DarkViolet"
+            $MaximoButton.BackColor = "DarkViolet"
+            $ChecklistButton.BackColor = "DarkViolet"
             $Mainform.ForeColor = "LightGray"
         }
         
